095e090c0c6a00a2acce064a390345fbc1ee38f3
long le <longdt@live.com>
2021-10-21 23:08:33 +0700

    x

--- readmodel.py
+++ readmodel.py
@@ -1,12 +1,19 @@
+
 import asyncio
 from bleak import BleakClient
 
-address = "FF:FF:38:5C:1C:3F"
+address = "ff:ff:38:5c:1c:3f"
 MODEL_NBR_UUID = "00002a24-0000-1000-8000-00805f9b34fb"
 
 async def main(address):
-    async with BleakClient(address) as client:
+    client = BleakClient(address)
+    try:
+        await client.connect()
         model_number = await client.read_gatt_char(MODEL_NBR_UUID)
         print("Model Number: {0}".format("".join(map(chr, model_number))))
+    except Exception as e:
+        print(e)
+    finally:
+        await client.disconnect()
 
 asyncio.run(main(address))
